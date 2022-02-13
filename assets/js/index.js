c4cb5cf8005404a73c374a83712e349914e44b2e
kohakubaibai <51185142+kohakubaibai@users.noreply.github.com>
2022-02-13 15:32:05 +0800

    強制push master

--- assets/js/index.js
+++ assets/js/index.js
@@ -7,12 +7,12 @@ $(function(){
 	var noSlides = $(".section").length;
 	var slideWidth = $(".section").width();
 	var slideContainerWidth = slideWidth * noSlides - ww;
-  
+  	var scrollIng = 0;
 	var actionHorizontal = new TimelineMax().to("#slideContainer", 1, {
 	  x: -slideContainerWidth,
 	  ease: Power0.easeNone
 	});
-  
+  var nowCurrentPos = 0;
 	var horizontal = createHorizontal();
 	function createHorizontal() {
 	  return new ScrollMagic.Scene({
@@ -25,11 +25,36 @@ $(function(){
 		.setTween(actionHorizontal)
 		.addTo(controller)
 		.on("update", function (e) {
-			var sceneAmount = 9,
-				sceneWidth= (e.endPos) / (sceneAmount-1),
+			var sceneAmount = 10,
+				sceneWidth= (e.endPos) / (sceneAmount),
 				currentPos=e.scrollPos;
 			var getScene = Math.round(currentPos/sceneWidth);
-			$('#anchor'+ getScene).addClass('active').siblings().removeClass('active')
+
+			if (getScene > 0) {
+				$('.navigation').fadeIn();
+				var anchorID = getScene - 1 ;
+				$('#anchor'+ anchorID).addClass('active').siblings().removeClass('active')
+			} else {
+				$('.navigation').fadeOut();
+			}
+			// DC
+			if (Math.abs(scrollIng - currentPos) < 100) scrollIng = 0;
+
+			if (getScene >= 0 && scrollIng == 0) {
+				var nextScene = Math.floor((currentPos - 50)/sceneWidth);
+
+				if (nowCurrentPos > currentPos) {
+					nextScene = Math.floor((currentPos + 25)/sceneWidth) - 1;
+				}
+
+				if (nextScene >= 0) {
+					$targetPos = $('#section-' + nextScene).offset().top;
+					$targetPos += $('#section-' + nextScene).offset().left;
+					controller.scrollTo($targetPos);
+					$(this).addClass('active').siblings().removeClass('active');
+					nowCurrentPos = currentPos;
+				}
+			}
 		});
 	}
   
@@ -59,8 +84,10 @@ $(function(){
 	  $targetPos += $(id).offset().left;
 	  if ($(id).length > 0) {
 		e.preventDefault();
+		// DC
+		scrollIng = $targetPos;
 		// trigger scroll
-		controller.scrollTo($targetPos);
+		controller.scrollTo($targetPos, true);
 		$(this).addClass('active').siblings().removeClass('active');
 	  }
 	});
