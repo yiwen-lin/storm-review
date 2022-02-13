b4aea8357122b1f28a79089d587c467737af6596
hwangiris <42462259+hwangiris@users.noreply.github.com>
2022-02-13 23:47:19 +0800

    navigation

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,8 +2,21 @@ let width = $(window).width();
 let index = null;
 let activeLink = null;
 
-document.getElementById('slideContainer').addEventListener("scroll", (e) => {
+const sliderContainer = document.getElementById('slideContainer');
+
+// $('html, body, *').mousewheel(function(e, delta) {
+//   this.scrollLeft -= (delta);
+//   console.log(e);
+// });
+  
+sliderContainer.addEventListener("scroll", (e) => {
 	const scrollLeft = e.target.scrollLeft;
 	index = Math.ceil(scrollLeft / width);
+
+	if ( index > 0 ) {
+		$('.navigation').fadeIn();
+	} else {
+		$('.navigation').fadeOut();
+	}
 	$('a.anchor-nav-item').removeClass('active').eq(index-1).addClass('active');
 });
