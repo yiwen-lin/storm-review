4794ae35c732b1f1dcaae12e143bc57a52b4d984
hwangiris <42462259+hwangiris@users.noreply.github.com>
2022-02-14 14:28:19 +0800

    滾動測試

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,21 +2,50 @@ let width = $(window).width();
 let index = null;
 let activeLink = null;
 
-const sliderContainer = document.getElementById('slideContainer');
+var sliderIndex = 0
 
-// $('html, body, *').mousewheel(function(e, delta) {
-//   this.scrollLeft -= (delta);
-//   console.log(e);
-// });
-  
-sliderContainer.addEventListener("scroll", (e) => {
-	const scrollLeft = e.target.scrollLeft;
-	index = Math.ceil(scrollLeft / width);
-
-	if ( index > 0 ) {
-		$('.navigation').fadeIn();
-	} else {
-		$('.navigation').fadeOut();
-	}
-	$('a.anchor-nav-item').removeClass('active').eq(index-1).addClass('active');
+window.addEventListener('load', function() {
+	var scrollStatus = {
+		wheeling: false,
+		functionCall: false
+	};
+	var scrollTimer = false;
+	window.addEventListener('wheel', function(e) {
+		scrollStatus.wheeling = true;
+		if (!scrollStatus.functionCall) {
+			//parseScroll here
+			if (e.deltaY > 0 && sliderIndex < 10) {
+				sliderIndex ++;
+			}
+			if (e.deltaY < 0 && sliderIndex > 0 ) {
+				sliderIndex --;
+			}
+			scrollStatus.functionCall = true;
+			console.log(width * sliderIndex, sliderIndex);
+			$('#js-wrapper').animate({
+				scrollLeft: width * sliderIndex
+			}, 500);
+			if ( sliderIndex > 0 ) {
+				$('.navigation').fadeIn();
+			} else {
+				$('.navigation').fadeOut();
+			}
+			if ( sliderIndex == 7 ) {
+				$('a.anchor-nav-item').eq(6).addClass('active');
+			} else {
+				$('a.anchor-nav-item').removeClass('active').eq(sliderIndex-1).addClass('active');
+			}
+		}
+		window.clearInterval(scrollTimer);
+		scrollTimer = window.setTimeout(function() {
+			scrollStatus.wheeling = false;
+			scrollStatus.functionCall = false;
+		}, 100); //set this millisecond to your liking
+	});
 });
+
+$('a.anchor-nav-item').click(function() {
+	$(this).siblings().removeClass('active');
+	$(this).addClass('active');
+	sliderIndex = $(this).index();
+})
